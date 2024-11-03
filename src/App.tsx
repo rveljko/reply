fa171e621331a0fed950064d91622c13bc1299f7
Veljko <radisavljevicveljko123@gmail.com>
2024-11-03 16:27:54 +0100

    create transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './layouts/app-layout/app-layout'
 import { dashboardRoutes } from './utils/routes'
 import { DASHBOARD_ROUTE } from './utils/constants'
+import TransactionsContextProvider from './utils/contexts/transactions-context'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path={DASHBOARD_ROUTE} element={<AppLayout />}>
-            {dashboardRoutes.map(({ id, href, page: Page }) => (
-              <Route key={id} path={href} element={<Page />} />
-            ))}
-          </Route>
-        </Routes>
+        <TransactionsContextProvider>
+          <Routes>
+            <Route path={DASHBOARD_ROUTE} element={<AppLayout />}>
+              {dashboardRoutes.map(({ id, href, page: Page }) => (
+                <Route key={id} path={href} element={<Page />} />
+              ))}
+            </Route>
+          </Routes>
+        </TransactionsContextProvider>
       </BrowserRouter>
     </>
   )
---
+++ src/utils/contexts/transactions-context.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { transactions as transactionsData } from '../../data/transactions'
+import { Transaction } from '../types'
+
+type TransactionsContextProviderProps = {
+  children: React.ReactNode
+}
+
+type TransactionsContextType = {
+  transactions: Transaction[]
+  addNewTransaction: (newTransaction: Transaction) => void
+}
+
+export const TransactionsContext =
+  createContext<TransactionsContextType | null>(null)
+
+function getInitialTransactions(): Transaction[] {
+  const transactions = localStorage.getItem('transactions')
+  return transactions ? JSON.parse(transactions) : transactionsData
+}
+
+export default function TransactionsContextProvider({
+  children,
+}: TransactionsContextProviderProps) {
+  const [transactions, setTransactions] = useState(getInitialTransactions)
+
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+  }, [transactions])
+
+  function addNewTransaction(newTransaction: Transaction) {
+    setTransactions((currentTransactions: Transaction[]) => [
+      newTransaction,
+      ...currentTransactions,
+    ])
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, addNewTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
+
+export const useTransactions = () => useContext(TransactionsContext)
