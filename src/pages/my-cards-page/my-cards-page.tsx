9ca880d223d0488c2bbf4e251c60e33b3c2a928b
Veljko <radisavljevicveljko123@gmail.com>
2024-11-25 15:51:39 +0100

    add recent transactions section to my cards page

--- src/pages/my-cards-page/my-cards-page.tsx
+++ src/pages/my-cards-page/my-cards-page.tsx
@@ -2,17 +2,21 @@ import BalanceExpensesSection from '../../sections/balance-expenses-section/bala
 import styles from './my-cards-page.module.css'
 import { useCreditCards } from '../../utils/contexts/credit-cards-context'
 import { CreditCard } from '../../utils/types'
+import RecentTransactionsSection from '../../sections/recent-transactions-section/recent-transactions-section'
+import { tableTransactionHeaders } from '../../data/transactions'
+import { useTransactions } from '../../utils/contexts/transactions-context'
 
 export default function MyCardsPage() {
   const { getCreditCardBalance, getCreditCardExpenses, getCreditCardById } =
     useCreditCards()
 
+  const { transactions } = useTransactions()
   const creditCard = getCreditCardById(1)
 
   return (
-    <div>
-      <div>
-        <div className={styles.tertiaryLayout}>
+    <div className={`${styles.layout} ${styles.vertical}`}>
+      <div className={`${styles.layout} ${styles.horizontal}`}>
+        <div className={`${styles.layout} ${styles.vertical}`}>
           <BalanceExpensesSection
             title="Balance"
             amount={getCreditCardBalance(creditCard as CreditCard)}
@@ -22,6 +26,13 @@ export default function MyCardsPage() {
             amount={getCreditCardExpenses(creditCard as CreditCard)}
           />
         </div>
+      </div>
+      <div className={`${styles.layout} ${styles.horizontal}`}>
+        <RecentTransactionsSection
+          tableTransactionHeaders={tableTransactionHeaders}
+          transactions={transactions.slice(0, 5)}
+          isLinkable
+        />
       </div>
     </div>
   )
