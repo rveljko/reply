9129700d9f195af085ac67e4c0642f7f8b1f3f24
Veljko <radisavljevicveljko123@gmail.com>
2024-11-29 15:25:08 +0100

    allow credit cards pagination via keyboard shortcuts

--- src/components/credit-cards-pagination/credit-cards-pagination.tsx
+++ src/components/credit-cards-pagination/credit-cards-pagination.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import ChevronLeftIcon from '../../icons/chevron-left-icon'
 import ChevronRightIcon from '../../icons/chevron-right-icon'
 import { CreditCard } from '../../utils/types'
@@ -14,18 +15,41 @@ export default function CreditCardPagination({
   creditCardIndex,
   setCreditCardIndex,
 }: CreditCardPaginationProps) {
+  function decrementCreditCardIndex() {
+    setCreditCardIndex((prevCreditCardIndex) =>
+      prevCreditCardIndex <= 0
+        ? (prevCreditCardIndex = creditCards.length - 1)
+        : prevCreditCardIndex - 1
+    )
+  }
+
+  function incrementCreditCardIndex() {
+    setCreditCardIndex((prevCreditCardIndex) =>
+      prevCreditCardIndex >= creditCards.length - 1
+        ? (prevCreditCardIndex = 0)
+        : prevCreditCardIndex + 1
+    )
+  }
+
+  function handleOnKeyDown(e: KeyboardEvent) {
+    if (e.code === 'ArrowLeft') decrementCreditCardIndex()
+    if (e.code === 'ArrowRight') incrementCreditCardIndex()
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', handleOnKeyDown)
+
+    return () => {
+      document.body.removeEventListener('keydown', handleOnKeyDown)
+    }
+  }, [])
+
   return (
     <ul className={styles.pagination}>
       <li>
         <button
           className={styles.button}
-          onClick={() => {
-            setCreditCardIndex((prevCreditCardIndex) =>
-              prevCreditCardIndex <= 0
-                ? (prevCreditCardIndex = creditCards.length - 1)
-                : prevCreditCardIndex - 1
-            )
-          }}
+          onClick={decrementCreditCardIndex}
           disabled={creditCards.length === 1}
         >
           <ChevronLeftIcon />
@@ -36,11 +60,7 @@ export default function CreditCardPagination({
           {creditCards.map((_, index) => (
             <li className={styles.item} key={index}>
               <button
-                onClick={() =>
-                  setCreditCardIndex(
-                    (prevCreditCardIndex) => (prevCreditCardIndex = index)
-                  )
-                }
+                onClick={() => setCreditCardIndex(index)}
                 className={`${styles.circle} ${
                   creditCardIndex === index ? styles.active : ''
                 }`}
@@ -52,13 +72,7 @@ export default function CreditCardPagination({
       <li>
         <button
           className={styles.button}
-          onClick={() => {
-            setCreditCardIndex((prevCreditCardIndex) =>
-              prevCreditCardIndex >= creditCards.length - 1
-                ? (prevCreditCardIndex = 0)
-                : prevCreditCardIndex + 1
-            )
-          }}
+          onClick={incrementCreditCardIndex}
           disabled={creditCards.length === 1}
         >
           <ChevronRightIcon />
