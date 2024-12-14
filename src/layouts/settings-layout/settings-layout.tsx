38aad2d3c5b9739805ab195ce668e5fe47d0cf9c
Veljko <radisavljevicveljko123@gmail.com>
2024-12-14 20:29:12 +0100

    add settings nav bar

--- src/layouts/settings-layout/settings-layout.tsx
+++ src/layouts/settings-layout/settings-layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import SettingsNavBar from '../../components/settings-nav-bar/settings-nav-bar'
+import styles from './settings-layout.module.css'
 
 export default function SettingsLayout() {
   return (
     <>
-      <div>links</div>
-      <Outlet />
+      <div className={styles.layout}>
+        <SettingsNavBar />
+        <Outlet />
+      </div>
     </>
   )
 }
