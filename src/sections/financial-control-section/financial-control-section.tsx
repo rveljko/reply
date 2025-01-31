9d8e59297ca4c3d16c5cf3bb0cbf8304b40552e1
Veljko <radisavljevicveljko123@gmail.com>
2025-01-31 09:06:05 +0100

    move feature cells into feature cell list component

--- src/sections/financial-control-section/financial-control-section.tsx
+++ src/sections/financial-control-section/financial-control-section.tsx
@@ -1,8 +1,6 @@
-import { motion } from 'motion/react'
-import FeatureCell from '../../components/feature-cell/feature-cell'
 import LandingPageSection from '../landing-page-section/landing-page-section'
 import { financialControlFeatures } from '../../data/features'
-import styles from './financial-control-section.module.css'
+import FeatureCellList from '../../components/feature-cell-list/feature-cell-list'
 
 export default function FinancialControlSection() {
   return (
@@ -10,25 +8,7 @@ export default function FinancialControlSection() {
       heading="Comprehensive Fraud Protection and Valuable Spending Insights"
       paragraph="Safeguard your finances with robust security measures that protect against fraud while gaining insights into your spending trends"
     >
-      <ul className={styles.content}>
-        {financialControlFeatures.map(
-          ({ id, icon: Icon, title, description }) => (
-            <motion.li
-              key={id}
-              initial={{ opacity: 0, transform: 'translateY(-2rem)' }}
-              whileInView={{ opacity: 1, transform: 'translateY(0)' }}
-              viewport={{ once: true }}
-              transition={{ delay: 0.1 * id }}
-            >
-              <FeatureCell
-                icon={<Icon />}
-                title={title}
-                description={description}
-              />
-            </motion.li>
-          )
-        )}
-      </ul>
+      <FeatureCellList features={financialControlFeatures} />
     </LandingPageSection>
   )
 }
--- src/sections/financial-management-solutions/financial-management-solutions.tsx
+++ src/sections/financial-management-solutions/financial-management-solutions.tsx
@@ -1,8 +1,6 @@
-import FeatureCell from '../../components/feature-cell/feature-cell'
-import { financialFeatures } from '../../data/features'
 import LandingPageSection from '../landing-page-section/landing-page-section'
-import styles from './financial-management-solutions.module.css'
-import { motion } from 'motion/react'
+import FeatureCellList from '../../components/feature-cell-list/feature-cell-list'
+import { financialFeatures } from '../../data/features'
 
 export default function FinancialManagementSolutions() {
   return (
@@ -10,23 +8,7 @@ export default function FinancialManagementSolutions() {
       heading="Streamlining Your Financial Management Process Effortlessly"
       paragraph="Simplify the management of your finances and effortlessly gain valuable insights to make informed decisions with ease"
     >
-      <ul className={styles.content}>
-        {financialFeatures.map(({ id, icon: Icon, title, description }) => (
-          <motion.li
-            key={id}
-            initial={{ opacity: 0, transform: 'translateY(-2rem)' }}
-            whileInView={{ opacity: 1, transform: 'translateY(0)' }}
-            viewport={{ once: true }}
-            transition={{ delay: 0.1 * id }}
-          >
-            <FeatureCell
-              icon={<Icon />}
-              title={title}
-              description={description}
-            />
-          </motion.li>
-        ))}
-      </ul>
+      <FeatureCellList features={financialFeatures} />
     </LandingPageSection>
   )
 }
