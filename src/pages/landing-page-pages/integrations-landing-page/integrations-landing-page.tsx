9c612117e6d67441c0eff415e73519637a2128c5
Veljko <radisavljevicveljko123@gmail.com>
2025-02-05 19:01:00 +0100

    add integrations section to integrations landing page

--- src/pages/landing-page-pages/integrations-landing-page/integrations-landing-page.tsx
+++ src/pages/landing-page-pages/integrations-landing-page/integrations-landing-page.tsx
@@ -1,3 +1,14 @@
+import { Helmet } from 'react-helmet-async'
+import IntegrationsSection from '../../../sections/landing-page-sections/integrations-section/integrations-section'
+import { TITLE_PREFIX } from '../../../utils/constants'
+
 export default function IntegrationsPage() {
-  return <div>integrations landing page</div>
+  return (
+    <>
+      <Helmet>
+        <title>{TITLE_PREFIX} Integrations</title>
+      </Helmet>
+      <IntegrationsSection />
+    </>
+  )
 }
